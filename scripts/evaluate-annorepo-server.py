971aba6a7abcee76e25594567a95571854e80cce
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2022-04-13 16:21:55 +0200

    refactor server evaluation script

--- scripts/evaluate-annorepo-server.py
+++ scripts/evaluate-annorepo-server.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 import argparse
 
+from colorama import Fore
+
 from annorepo.client import AnnoRepoClient
+
+success_counter = 0
+failure_counter = 0
+
+
+def evaluate_task(name, method):
+    global failure_counter, success_counter
+    print(f"{Fore.YELLOW}> {Fore.CYAN}{name}{Fore.YELLOW}:{Fore.RESET}")
+    try:
+        result = method()
+        print(f"{Fore.GREEN}success{Fore.RESET}")
+        success_counter += 1
+        print(f"{Fore.BLUE}method returned:{Fore.RESET}")
+        print(result)
+    except Exception:
+        failure_counter += 1
+        print(f"{Fore.RED}failure{Fore.RESET}")
+    print()
 
 
 def main():
@@ -14,34 +34,21 @@ def main():
     args = parser.parse_args()
 
     if args.url:
-        print(f"evaluating {args.url}")
-        client = AnnoRepoClient(args.url)
+        print(f"{Fore.BLUE}evaluating {Fore.YELLOW}{args.url}{Fore.RESET}")
+        print()
+        client = AnnoRepoClient(args.url, verbose=True)
 
-        about = client.get_about()
-        print("> get_about:")
-        print(about)
+        evaluate_task('get_about', client.get_about)
+        evaluate_task('get_homepage', client.get_homepage)
+        evaluate_task('get_robots_txt', client.get_robots_txt)
+        evaluate_task('get_swagger_json', client.get_swagger_json)
+        evaluate_task('get_swagger_yaml', client.get_swagger_yaml)
 
-        homepage = client.get_homepage()
-        print("> get_homepage:")
-        print(homepage)
-
-        robots = client.get_robots_txt()
-        print("> get_robots_txt:")
-        print(robots)
-
-        # favicon = client.get_favicon()
-        # print("> get_favicon:")
-        # print(favicon)
-
-        swagger = client.get_swagger_json()
-        print("> get_swagger_json:")
-        print(swagger)
-
-        swagger = client.get_swagger_yaml()
-        print("> get_swagger_yaml:")
-        print(swagger)
-
-        print("done!")
+        print()
+        print(f"{Fore.GREEN}{success_counter} successes{Fore.RESET}")
+        print(f"{Fore.RED}{failure_counter} failures{Fore.RESET}")
+        print()
+        print(f"{Fore.BLUE}done!{Fore.RESET}")
 
 
 if __name__ == '__main__':
